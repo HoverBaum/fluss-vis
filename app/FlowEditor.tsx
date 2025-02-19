987aaea57cc6f1deb6b84d7c2def9d2a24ad2f49
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2025-02-19 14:53:38 +0100

    🎉 basic custom node

---
+++ app/FlowEditor.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import React, { useCallback, useMemo } from 'react'
+import {
+  addEdge,
+  Background,
+  BackgroundVariant,
+  Connection,
+  Controls,
+  MiniMap,
+  ReactFlow,
+  useEdgesState,
+  useNodesState,
+} from '@xyflow/react'
+
+import '@xyflow/react/dist/style.css'
+import { CustomNode } from './CustomNode'
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  { id: '3', position: { x: 0, y: 200 }, type: 'custom', data: {} },
+]
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }]
+
+export const FlowEditor = () => {
+  const [nodes, , onNodesChange] = useNodesState(initialNodes)
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
+
+  const onConnect = useCallback(
+    (params: Connection) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  )
+
+  const nodeTypes = useMemo(() => ({ custom: CustomNode }), [])
+
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        nodeTypes={nodeTypes}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        <Controls />
+        <MiniMap />
+        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+      </ReactFlow>
+    </div>
+  )
+}
