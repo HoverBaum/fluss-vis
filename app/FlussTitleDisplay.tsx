e2822307d5d387a7adf773dc15af58f4ea408e91
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2025-02-25 16:56:29 +0100

    ✨ Fluss has an editable name

--- app/FlussTitleDisplay.tsx
+++ app/FlussTitleDisplay.tsx
@@ -1,9 +1,50 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import { useFlussStore } from '@/stores/FlussStoreProvider'
+import { useState } from 'react'
 
 export const FlussTitleDisplay = () => {
   const name = useFlussStore((store) => store.name)
+  const rename = useFlussStore((store) => store.rename)
+  const [isEditing, setIsEditing] = useState(false)
+  const [newName, setNewName] = useState(name)
 
-  return <div className="p-2 px-4 bg-background shadow-md">{name}</div>
+  const cancelEditing = () => {
+    setIsEditing(false)
+    setNewName(name)
+  }
+
+  return (
+    <div className="p-2 px-4 bg-background shadow-md">
+      {!isEditing && <span onClick={() => setIsEditing(true)}>{name}</span>}
+      {isEditing && (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            setIsEditing(false)
+            rename(newName)
+          }}
+        >
+          <Input
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+            autoFocus
+            onKeyDown={(e) => {
+              if (e.key === 'Escape') {
+                cancelEditing()
+              }
+            }}
+          />
+          <div className="mt-2 grid grid-cols-2 gap-2">
+            <Button type="submit">Save</Button>
+            <Button onClick={cancelEditing} variant="secondary">
+              Cancel
+            </Button>
+          </div>
+        </form>
+      )}
+    </div>
+  )
 }
