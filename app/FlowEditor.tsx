dc06cb5be95045348fb82ce64c1936c2f890f534
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2025-02-25 18:19:45 +0100

    🌗 dark mode

--- app/FlowEditor.tsx
+++ app/FlowEditor.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useCallback, useMemo } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import {
   addEdge,
   Background,
@@ -18,6 +18,8 @@ import {
 import '@xyflow/react/dist/style.css'
 import { RiparianNode } from './RiparianNode'
 import { FlussTitleDisplay } from './FlussTitleDisplay'
+import { ModeToggle } from '@/components/ModeToggle'
+import { useTheme } from 'next-themes'
 
 const initialNodes = [
   {
@@ -40,6 +42,13 @@ const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }]
 export const FlowEditor = () => {
   const [nodes, , onNodesChange] = useNodesState(initialNodes)
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
+  const [isMounted, setIsMounted] = useState(false)
+  const { theme } = useTheme()
+
+  // Hack to make sure we know the clients theme and only render on client.
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
 
   const onConnect = useCallback(
     (params: Connection) => setEdges((eds) => addEdge(params, eds)),
@@ -47,6 +56,8 @@ export const FlowEditor = () => {
   )
 
   const nodeTypes = useMemo(() => ({ custom: RiparianNode }), [])
+
+  if (!isMounted) return null
 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
@@ -57,9 +68,14 @@ export const FlowEditor = () => {
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        colorMode={theme as 'system' | 'light' | 'dark'}
+        suppressHydrationWarning
       >
         <Panel position="top-center">
           <FlussTitleDisplay />
+        </Panel>
+        <Panel position="top-right">
+          <ModeToggle />
         </Panel>
         <Controls />
         <MiniMap />
