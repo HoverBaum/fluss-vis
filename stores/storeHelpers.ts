6d23a0f06d6eb872a92b0006ab59fdd6f796cd3f
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2025-03-12 11:46:55 +0100

    ✨ update add Node

--- stores/storeHelpers.ts
+++ stores/storeHelpers.ts
@@ -1,21 +1,29 @@
 import { nanoid } from 'nanoid'
 import { Position, XYPosition } from '@xyflow/react'
-import { FlussNodeData, FlussNodeType } from './flussStore'
+import { FlussNodeType } from './flussStore'
+import { FlussStep } from '@/fluss-lib/fluss'
 
 export const START_NODE_ID = 'start'
 export const END_NODE_ID = 'end'
 
 export const createFlussNode = (
   position: XYPosition,
-  data?: FlussNodeData
+  data?: FlussStep
 ): FlussNodeType => {
   // Make sure that each new node has at least a name.
-  const nodeData = data || { name: 'New Node', inputs: [] }
-  if (!nodeData.inputs) {
-    nodeData.inputs = []
-  }
-  if (!nodeData.name) {
-    nodeData.name = 'New Node'
+  const nodeData = data || {
+    type: 'step',
+    id: nanoid(5),
+    description: '',
+    name: 'New Node',
+    inputs: [],
+    outputs: [
+      {
+        id: nanoid(5),
+        name: 'Unnamed',
+        type: 'void',
+      },
+    ],
   }
 
   return {
