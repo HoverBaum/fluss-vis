c5f641b2b14cd40baec7412684b8c78ec6c64281
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2025-03-13 22:23:51 +0100

    ✨ code display for export

--- app/Export.tsx
+++ app/Export.tsx
@@ -1,5 +1,6 @@
 import { Copy, Download } from 'lucide-react'
-
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
+import ts from 'react-syntax-highlighter/dist/esm/languages/hljs/typescript'
 import {
   Dialog,
   DialogClose,
@@ -11,8 +12,21 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button'
+import {
+  a11yDark,
+  a11yLight,
+} from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import { useTheme } from 'next-themes'
+import { codeMock } from './codeMock'
+
+SyntaxHighlighter.registerLanguage('typescript', ts)
 
 export const Export = () => {
+  const { theme, systemTheme } = useTheme()
+  const isDark =
+    theme === 'dark' || (theme === 'system' && systemTheme === 'dark')
+  const codeToCopy = codeMock
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -20,7 +34,7 @@ export const Export = () => {
           Export <Download />
         </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-md">
+      <DialogContent className="sm:max-w-lg">
         <DialogHeader>
           <DialogTitle>Fluss Export</DialogTitle>
           <DialogDescription>
@@ -30,12 +44,13 @@ export const Export = () => {
         </DialogHeader>
 
         <div>
-          <pre>
-            <code>type Fluss = {}</code>
-          </pre>
-          <Button size="icon">
-            <Copy />
-          </Button>
+          <SyntaxHighlighter
+            className="max-h-80"
+            language="typescript"
+            style={isDark ? a11yDark : a11yLight}
+          >
+            {codeToCopy}
+          </SyntaxHighlighter>
         </div>
 
         <DialogFooter className="sm:justify-start">
@@ -44,6 +59,15 @@ export const Export = () => {
               Close
             </Button>
           </DialogClose>
+          <Button
+            size="icon"
+            className="w-full"
+            onClick={() => {
+              navigator.clipboard.writeText(codeToCopy)
+            }}
+          >
+            <Copy />
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
---
+++ app/codeMock.ts
@@ -0,0 +1,19 @@
+export const codeMock = `type MyType = string;
+
+/**
+ * Much fake, so wow.
+ * Long comment
+ */
+export const fluss = {
+  run: (code: string) => {
+    return code
+  }
+};
+
+export AnotherType = {
+  name: string;
+  age: number;
+  id: string;
+  property: MyType;
+}
+`
