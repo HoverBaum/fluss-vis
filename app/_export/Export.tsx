d5e1e31f78ebb389ed6c490a7d04f04be5047f06
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2025-03-25 11:55:14 +0100

    ✨ animation for code export

--- app/_export/Export.tsx
+++ app/_export/Export.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'motion/react'
 import { Copy, Download } from 'lucide-react'
 import {
   Dialog,
@@ -12,12 +13,19 @@ import {
 import { Button } from '@/components/ui/button'
 import { toast } from 'sonner'
 import { useExport } from './useExport'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { CodeDisplay } from '@/components/CodeDisplay'
+import codeAnimation from './code_animation.json'
+import Lottie from 'lottie-react'
 
 export const Export = () => {
   const [codeToCopy, setCodeToCopy] = useState('')
   const { flussExport } = useExport()
+  const [animationDone, setAnimationDone] = useState(false)
+
+  useEffect(() => {
+    setAnimationDone(false)
+  }, [codeToCopy])
 
   return (
     <Dialog onOpenChange={(open) => open && flussExport().then(setCodeToCopy)}>
@@ -34,28 +42,47 @@ export const Export = () => {
             *.fluss.ts.
           </DialogDescription>
         </DialogHeader>
+        {!animationDone && (
+          <div>
+            <Lottie
+              animationData={codeAnimation}
+              className="h-60"
+              loop={false}
+              onComplete={() => setTimeout(() => setAnimationDone(true), 500)}
+            />
+            <p className="text-center text-lg">Creating your Fluss 🪄</p>
+          </div>
+        )}
 
-        <div className="overflow-scroll w-full max-h-80 border border-border">
-          <CodeDisplay>{codeToCopy}</CodeDisplay>
-        </div>
-
-        <DialogFooter className="sm:justify-start">
-          <DialogClose asChild>
-            <Button type="button" variant="secondary">
-              Close
-            </Button>
-          </DialogClose>
-          <Button
-            size="icon"
-            className="w-full"
-            onClick={() => {
-              navigator.clipboard.writeText(codeToCopy)
-              toast('Copied to clipboard')
-            }}
+        {animationDone && (
+          <motion.div
+            initial={{ height: 0 }}
+            animate={{ height: 'auto' }}
+            className="overflow-scroll w-full max-h-80 border border-border"
           >
-            <Copy /> Copy Code
-          </Button>
-        </DialogFooter>
+            <CodeDisplay>{codeToCopy}</CodeDisplay>
+          </motion.div>
+        )}
+
+        {animationDone && (
+          <DialogFooter className="sm:justify-start">
+            <DialogClose asChild>
+              <Button type="button" variant="secondary">
+                Close
+              </Button>
+            </DialogClose>
+            <Button
+              size="icon"
+              className="w-full"
+              onClick={() => {
+                navigator.clipboard.writeText(codeToCopy)
+                toast('Copied to clipboard')
+              }}
+            >
+              <Copy /> Copy Code
+            </Button>
+          </DialogFooter>
+        )}
       </DialogContent>
     </Dialog>
   )
