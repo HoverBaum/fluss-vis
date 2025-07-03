02b41799d36ad2ea844843a980ecf61cac89cf95
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2025-07-03 20:29:14 +0200

    🎨 new icon for system mode.

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { LaptopIcon, MoonIcon, SmartphoneIcon, SunIcon } from 'lucide-react'
+import { MoonIcon, SunIcon, SunMoonIcon } from 'lucide-react'
 import { useTheme } from 'next-themes'
 
 import { Button } from '@/components/ui/button'
@@ -10,35 +10,17 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
-import { useMemo } from 'react'
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme()
-
-  // Define the Icon to be used for system based on the users system.
-  const SystemIcon = useMemo(() => {
-    const userAgent = navigator.userAgent
-    // Basic check for mobile devices
-    if (
-      /android|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(
-        userAgent.toLowerCase()
-      )
-    ) {
-      return SmartphoneIcon
-    } else {
-      return LaptopIcon
-    }
-  }, [])
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          {theme === 'light' && <SunIcon className="h-[1.2rem] w-[1.2rem]" />}
-          {theme === 'dark' && <MoonIcon className="h-[1.2rem] w-[1.2rem]" />}
-          {theme === 'system' && (
-            <SystemIcon className="h-[1.2rem] w-[1.2rem]" />
-          )}
+          {theme === 'light' && <SunIcon />}
+          {theme === 'dark' && <MoonIcon />}
+          {theme === 'system' && <SunMoonIcon />}
 
           <span className="sr-only">Toggle theme</span>
         </Button>
@@ -51,7 +33,7 @@ export function ModeToggle() {
           <MoonIcon /> Dark
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme('system')}>
-          <SystemIcon /> System
+          <SunMoonIcon /> System
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
