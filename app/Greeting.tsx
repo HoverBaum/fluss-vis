0bccf1fb3a5d51fbf902299a2c8bcbe452fe58af
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2025-08-08 14:45:32 +0200

    📚 spelling

--- app/Greeting.tsx
+++ app/Greeting.tsx
@@ -18,7 +18,7 @@ export const Greeting = ({ showGreeting }: GreetingProps) => {
           exit={{ opacity: 0, scale: 0 }}
           className="cursor-default p-4"
         >
-          <h1 className="text-center text-5xl">Fluss-Vis</h1>
+          <h1 className="text-center text-5xl">Fluss Vis</h1>
           <div className="flex justify-center p-4">
             <div className="grid grid-cols-2 gap-8">
               <div className="grid place-items-center">
